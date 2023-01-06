74f72b9b63388ba3f597f45dd790256348a46ac2
deadaf <gahlyanrohit18@gmail.com>
2023-01-06 23:22:04 +0530

    calculator html done

---
+++ calculator/calc.js
@@ -0,0 +1,4 @@
+document.getElementById("answer").readOnly = true; //set this attribute in Html file
+let screen = document.getElementById("answer");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
