ee1ca1a81258ffc0c7d10b20a63fc0aee26a4f56
deadaf <gahlyanrohit18@gmail.com>
2023-01-08 20:22:39 +0530

    add js logic to calculate things

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,4 +1,26 @@
 document.getElementById("answer").readOnly = true; //set this attribute in Html file
-let screen = document.getElementById("answer");
-buttons = document.querySelectorAll("button");
+
 let screenValue = "";
+buttons = document.querySelectorAll("button");
+
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+
+    if (buttonText == "=") {
+      try {
+        screenValue = eval(screenValue);
+        document.getElementById("answer").value = screenValue;
+      } catch (err) {
+        console.log("error");
+        document.getElementsById("errorMsg").innerHTML = err;
+      }
+    } else if (buttonText == "C") {
+      screenValue = "";
+      document.getElementById("answer").value = screenValue;
+    } else {
+      screenValue += buttonText;
+      document.getElementById("answer").value = screenValue;
+    }
+  });
+}
